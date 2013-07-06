0553ce27bb1db5af6fae16884a563bbddef2516c
Jeremy Stretch <stretch@packetlife.net>
2013-07-06 19:10:51 -0400

    Fleshing out labs views and templates

--- labkeeper/labs/urls.py
+++ labkeeper/labs/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'labs.views.default', name='labs'),
+urlpatterns = patterns('labs',
+    url(r'^$', 'views.default', name='labs'),
+    url(r'^(?P<id>\d+)$', 'views.lab', name='lab'),
 )
--- labkeeper/labs/views.py
+++ labkeeper/labs/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from labs.models import Lab
 
 def default(request):
 
-    return render(request, 'labs/default.html')
+    return render(request, 'labs/default.html', {
+        'labs': Lab.objects.filter(is_public=True),
+        })
+
+def lab(request, id):
+
+    lab = get_object_or_404(Lab, id=id)
+
+    return render(request, 'labs/lab.html', {
+        'lab': lab,
+        })
